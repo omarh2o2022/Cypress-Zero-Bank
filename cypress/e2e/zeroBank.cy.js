498669cc8dc6cb5cced8cfc5f5f902959a1602e7
omar <omarh2o@outlook.com>
2023-05-09 05:35:23 -0600

    cypress signin created

--- cypress/e2e/zeroBank.cy.js
+++ cypress/e2e/zeroBank.cy.js
@@ -12,13 +12,21 @@ describe (' Zero Bank', () => {
       })
 
 
-    it ('Account Summary', ()=> {
+    it ('Account Summary assert', ()=> {
         
         cy.get('.brand').should('have.text', 'Zero Bank') 
         cy.get('.offset2 > :nth-child(1)').should('have.text', 'Cash Accounts') 
         cy.get('.offset2 > :nth-child(3)').should('have.text', 'Investment Accounts')
         cy.get('.offset2 > :nth-child(5)').should('have.text', 'Credit Accounts')
         cy.get('.offset2 > :nth-child(7)').should('have.text', 'Loan Accounts')
+        cy.get(':nth-child(2) > .board-content > .table > thead > tr > :nth-child(3)').should('have.text', 'Balance')
+        
+    })
+
+    it('Account Activity', ()=>{
+        cy.get('#account_activity_tab > a').click()
+        cy.get('.board-header').should('have.text','Show Transactions')
+        cy.get('#aa_accountId').select([2])
     })
     
     
